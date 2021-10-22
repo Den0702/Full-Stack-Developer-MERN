9af3afd34433b07a1fd1a9ddb7d18aff5b616a4b
Dzianis Matsiushyn <ellan@tut.by>
2021-10-22 11:37:21 +0200

    suma-zmiennych completed

---
+++ JavsScript Prework/suma-zmiennych/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let firstNumber = 34;
+let secondNumber = 4.5;
+
+/* Wykorzystalem Function Expression */
+let sum = function(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+console.log(sum(firstNumber, secondNumber));
